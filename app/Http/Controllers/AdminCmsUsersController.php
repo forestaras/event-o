dc39745537fd8cf5acd0d3ebd191e07b13249f05
forestaras <forestaras@gmail.com>
2023-02-26 23:31:39 +0200

    test

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -4,8 +4,9 @@ use Session;
 use Request;
 use DB;
 use CRUDbooster;
+use crocodicstudio\crudbooster\controllers\CBController;
 
-class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CBController {
+class AdminCmsUsersController extends CBController {
 
 
 	public function cbInit() {
@@ -20,24 +21,24 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 	
 		# START COLUMNS DO NOT REMOVE THIS LINE
 		$this->col = array();
-		$this->col[] = array("label"=>"Name","name"=>"name");
+		$this->col[] = array("label"=>"Імя","name"=>"name");
 		$this->col[] = array("label"=>"Email","name"=>"email");
 		$this->col[] = array("label"=>"Privilege","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
-		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>1);		
+		$this->col[] = array("label"=>"Фото","name"=>"photo","image"=>1);		
 		# END COLUMNS DO NOT REMOVE THIS LINE
 
 		# START FORM DO NOT REMOVE THIS LINE
 		$this->form = array(); 		
-		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
+		$this->form[] = array("label"=>"Імя","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000','resize_width'=>90,'resize_height'=>90);											
+		$this->form[] = array("label"=>"Фото","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000','resize_width'=>90,'resize_height'=>90);											
 		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);	
 		// $this->form[] = ['label'=>'Спорсмен','name'=>'peopleid','type'=>'select2','width'=>'col-sm-10','datatable'=>'people,name'];					
 		// $this->form[] = ['label'=>'Спорсмен','name'=>'peopleid','type'=>'select2','width'=>'col-sm-10','datatable'=>'club,name'];					
 		// $this->form[] = ['label'=>'Спорсмен','name'=>'peopleid','type'=>'select2','width'=>'col-sm-10','datatable'=>'obl,title'];					
 		// $this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
-		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
-		$this->form[] = array("label"=>"Password Confirmation","name"=>"password_confirmation","type"=>"password","help"=>"Please leave empty if not change");
+		$this->form[] = array("label"=>"Пароль","name"=>"password","type"=>"password","help"=>"Будь ласка, залиште порожнім, якщо не змінюєте");
+		$this->form[] = array("label"=>"Повторити пароль","name"=>"password_confirmation","type"=>"password","help"=>"Будь ласка, залиште порожнім, якщо не змінюєте");
 		# END FORM DO NOT REMOVE THIS LINE
 				
 	}
@@ -50,10 +51,11 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 		$this->button_add     = FALSE;
 		$this->button_delete  = FALSE;	
 		$this->hide_form 	  = ['id_cms_privileges'];
-
+		
 		$data['page_title'] = trans("crudbooster.label_button_profile");
 		$data['row']        = CRUDBooster::first('cms_users',CRUDBooster::myId());		
-		$this->cbView('crudbooster::default.form',$data);				
+		// $data = array_merge($data, get_object_vars($this));
+		return $this->view('crudbooster::default.form',$data);			
 	}
 	public function hook_before_edit(&$postdata,$id) { 
 		unset($postdata['password_confirmation']);
