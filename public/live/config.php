074ba43e868b8fde3b51bc9a27c32234d6929834
forestaras <forestaras@gmail.com>
2023-02-28 00:03:14 +0200

    meos

--- public/live/config.php
+++ public/live/config.php
@@ -25,30 +25,3 @@ $dbname = "u320095004_evento";
   $lang['totafter'] = "&nbsp;";
 
 ?>
-
-
-
-<!-- define("MYSQL_HOSTNAME", "localhost");
-define("MYSQL_USERNAME", "u320095004_forestaras");
-define("MYSQL_DBNAME", "u320095004_evento");  
-define("MYSQL_PASSWORD", "Nfhnfr-30");
-
-define("MEOS_PASSWORD", "meos");
-$servername = "localhost";
-$username = "u320095004_forestaras";
-$password = "Nfhnfr-30";
-$dbname = "u320095004_evento";
-
-
- 
-  $lang = array();
-  $lang['selectcmp'] = "Select Competition";
-  $lang['place'] = "Place";
-  $lang['team'] = "Team";
-  $lang['name'] = "Name";
-  $lang['after'] = "&nbsp;";
-  $lang['time'] = "Time";
-  $lang['finish'] = "Finish";
-  $lang['tottime'] = "Total Time";
-  $lang['totafter'] = "&nbsp;"; -->
-
