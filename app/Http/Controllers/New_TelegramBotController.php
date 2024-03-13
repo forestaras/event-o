6a8c7d5ac08dd0b1892fee5f6fb8e900f8b1a220
Melnyk Taras <forestaras@gmail.com>
2024-03-13 09:13:15 +0200

    llll

--- app/Http/Controllers/New_TelegramBotController.php
+++ app/Http/Controllers/New_TelegramBotController.php
@@ -23,31 +23,6 @@ class New_TelegramBotController extends Controller
             $text = trim($data['message']['text']);
             $text_array = explode(" ", $text);
 
-            // получим текущее состояние бота, если оно есть
-            // $bot_state = self::get_bot_state ($chat_id);
-
-
-
-
-
-
-            // $text_return = "Ви додали, $text, до слідкування за результатами: 
-            //     /help - список команд
-            //     /about - о нас
-            //     /order - оставить заявку
-            //     ";
-
-            // message_to_telegram($bot_token, $order_chat_id, $text_return);
-            // set_bot_state ($chat_id, ''); // не забудем почистить состояние на пустоту, после отправки заявки
-
-
-
-
-
-            // если текущее состояние бота отправка заявки, то отправим заявку менеджеру компании на $order_chat_id
-
-
-            // вывод информации Помощь
             if ($text == '/help') {
                 $text_return = "Привет, $first_name $last_name, вот команды, что я понимаю: 
             /help - список команд
@@ -55,10 +30,8 @@ class New_TelegramBotController extends Controller
             /order - оставить заявку
             ";
                 self::message_to_telegram($bot_token, $chat_id, $text_return);
-                // self::set_bot_state ($chat_id, '/help');
             }
 
-            // вывод информации о нас
             elseif ($text == '/about') {
                 $text_return = "verysimple_bot:
             Я пример самого простого бота для телеграм, написанного на простом PHP.
@@ -66,28 +39,17 @@ class New_TelegramBotController extends Controller
             https://www.novelsite.ru/kak-sozdat-prostogo-bota-dlya-telegram-na-php.html
             ";
                 self::message_to_telegram($bot_token, $chat_id, $text_return);
-                // self::set_bot_state ($chat_id, '/about');
             }
 
-            // переход в режим Заявки
             elseif ($text == '/order') {
-
-
-
 
                 $text_return = "$first_name $last_name, для подтверждения Заявки введите текст вашей заявки и нажмите отправить. 
         Наши специалисты свяжутся с вами в ближайшее время!
         ";
                 self::message_to_telegram($bot_token, $chat_id, $text_return);
-                // self::set_bot_state ($chat_id, '/order');
             }
         }
 
-        // функция отправки сообщения от бота в диалог с юзером
-
-        // сохранить состояние бота для пользователя
-
-        // получить текущее состояние бота для пользователя
     }
     static function message_to_telegram($bot_token, $chat_id, $text, $reply_markup = '')
     {
@@ -107,20 +69,5 @@ class New_TelegramBotController extends Controller
 
         curl_setopt_array($ch, $ch_post);
         curl_exec($ch);
-    }
-
-
-    static function set_bot_state($chat_id, $data)
-    {
-        file_put_contents(__DIR__ . '/users/' . $chat_id . '.txt', $data);
-    }
-    static function get_bot_state($chat_id)
-    {
-        if (file_exists(__DIR__ . '/users/' . $chat_id . '.txt')) {
-            $data = file_get_contents(__DIR__ . '/users/' . $chat_id . '.txt');
-            return $data;
-        } else {
-            return '';
-        }
     }
 }
