2d77bf652a3ead6495a9db96b9424be65a6f1741
Melnyk Taras <forestaras@gmail.com>
2024-03-13 02:08:34 +0200

    dfg

--- app/Http/Controllers/TelegramController2.php
+++ app/Http/Controllers/TelegramController2.php
@@ -13,6 +13,10 @@ use App\Http\Middleware\VerifyCsrfToken;
 
 class TelegramController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(VerifyCsrfToken::class)->except('yourMethod');
+    }
 
     public function telegram(Request $request)
     {
