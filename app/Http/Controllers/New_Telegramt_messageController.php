590ffdbda40c743a6419bb849684168880f38527
Melnyk Taras <forestaras@gmail.com>
2024-03-13 17:27:45 +0200

    лгрг

--- app/Http/Controllers/New_Telegramt_messageController.php
+++ app/Http/Controllers/New_Telegramt_messageController.php
@@ -15,41 +15,50 @@ class New_Telegramt_messageController extends Controller
             $last_name = $data['message']['from']['last_name'];
             $text = trim($data['message']['text']);
             $text_array = explode(" ", $text);
+            $bot_state = self::get_bot_state($chat_id);
         }
-        switch ($text) {
-            case '/name':
-                $text_return = "Вкажіть імя яке ви хочете відслідковувати ";
+        if ($bot_state) {
+            if ($bot_state=='/name') {
+                $text_return = "Будьласка напишіть імя яке ви хочете добавити до списку.";
+            }
+            if ($bot_state=='/dele') {
+                $text_return = "Вкажіть імя яке ви хочете видалити зі списку";
+            }
+        } else {
+            switch ($text) {
+                case '/help':
+                    $text_return = "Доступні команди /name для добавлення імя до відслідковування
+                    /delet_name видалення
+                    /all_name перегляд всіх імен які відслідковуються";
+                    
+                    break;
 
-                break;
-            case '/start':
-                $text_return = "sjdhfbsjhdfbjdshbfk";
-                break;
-            case '/help':
-                $text_return = "Привет, $first_name $last_name, вот команды, что я понимаю: 
-        /help - список команд
-        /about - о нас
-        /order - оставить заявку
-        ";
-                break;
-            case '/about':
-                $text_return = "verysimple_bot:
-        Я пример самого простого бота для телеграм, написанного на простом PHP.
-        Мой код можно скачивать, дополнять, исправлять. Код доступен в этой статье:
-        https://www.novelsite.ru/kak-sozdat-prostogo-bota-dlya-telegram-na-php";
-                break;
-            case '/order':
-                $text_return = "$first_name $last_name, для подтверждения Заявки введите текст вашей заявки и нажмите отправить. 
-                    Наши специалисты свяжутся с вами в ближайшее время!
-                    ";
-                break;
-            default:
-            $text_return = "$first_name $last_name, Натисніть старт щоб розпочати /start
-            ";
-        break;
+                case '/info':
+                    $text_return = "Вкажіть імя яке ви хочете відслідковувати ";
+                    break;
+
+                case '/sta':
+                    $text_return = "Привіт я стартова сторінка";
+                    break;
+
+                default:
+                    $text_return = $text;
+                    break;
+            }
         }
-        
-        file_put_contents('telegram_bot/users/'.$chat_id . '.txt', $text);
+
+        file_put_contents('telegram_bot/users/' . $chat_id . '.txt', $text);
         return $text_return;
     }
 
+    static function get_bot_state($chat_id)
+    {
+        if (file_exists(__DIR__ . '/users/' . $chat_id . '.txt')) {
+            $data = file_get_contents(__DIR__ . '/users/' . $chat_id . '.txt');
+            $data = substr($data, 0, 5);
+            return $data;
+        } else {
+            return '';
+        }
+    }
 }
