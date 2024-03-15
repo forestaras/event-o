c5f590f3aca7cd7f04b9a831bd71d17cb381d0e7
Melnyk Taras <forestaras@gmail.com>
2024-03-16 01:20:43 +0200

    sss

--- app/Http/Controllers/New_TelegramBotController.php
+++ app/Http/Controllers/New_TelegramBotController.php
@@ -38,8 +38,8 @@ class New_TelegramBotController extends Controller
         foreach ($peoples as $people)  $name[] = $people->name;
         if ($name > 0) $telegram = Telegram::whereIn('name', $name)->get();// імена яким потріно відропавити результати
         foreach ($telegram as $t) {
-            $rezult=$peoples->where('name',$t->name);
-            $text_message="Хтось ваш $rezult";
+            $rezult=$peoples->where('name',$t->name)->first();
+            $text_message="Хтось ваш $rezult->name";
             self::message_to_telegram($t->user_id, $text_message, $reply_markup = '');
         }
         
