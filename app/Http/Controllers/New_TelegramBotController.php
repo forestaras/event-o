a4fa3d31c13a0ca855835e40505edb44985687bb
Melnyk Taras <forestaras@gmail.com>
2024-03-20 23:40:52 +0200

    dddd

--- app/Http/Controllers/New_TelegramBotController.php
+++ app/Http/Controllers/New_TelegramBotController.php
@@ -55,12 +55,15 @@ class New_TelegramBotController extends Controller
         
     }
 
-    public function curl($url){
+    public function curl(){
+        // $url = "http://example.com";
         $url = "https://event-o.net/api/telegram/rez/107";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
+        $response = file_get_contents($url);
+        
+        // Обробка відповіді (опціонально)
+        
+        return response()->json(['response' => $response]);
+
     }
     
     static function message_to_telegram($chat_id, $text, $reply_markup = '')
